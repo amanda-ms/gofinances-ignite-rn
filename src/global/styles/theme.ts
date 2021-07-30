a1ee9879ccae2addebaf08c29ae2f6365990a12e
Amanda Silva <amanda-ms@hotmail.com>
2021-07-30 16:42:52 -0300

    feat: create components select transaction type

--- src/global/styles/theme.ts
+++ src/global/styles/theme.ts
@@ -11,6 +11,7 @@ export default {
 
     title: "#363F5F",
     text: "#969CB2",
+    text_light: "#DEE1E9",
     text_dark: "#000",
     background: "#F0F2F5",
   },
--- src/pages/Register/index.tsx
+++ src/pages/Register/index.tsx
@@ -1,9 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import { Input } from "../../components/Form/Input";
 import { Button } from "../../components/Form/Button";
-import { Container, Header, Title, Form, Fields } from "./style";
+import { TransactionTypeButton } from "../../components/Form/TransactionTypeButton";
+import {
+  Container,
+  Header,
+  Title,
+  Form,
+  Fields,
+  TransactionTypes,
+} from "./style";
 
 export function Register() {
+  const [transactionType, setTransectionType] = useState("");
+
+  function handleTransactionType(type: "up" | "down") {
+    setTransectionType(type);
+  }
+
   return (
     <Container>
       <Header>
@@ -14,6 +28,20 @@ export function Register() {
         <Fields>
           <Input placeholder="Nome" />
           <Input placeholder="Preço" />
+          <TransactionTypes>
+            <TransactionTypeButton
+              isActive={transactionType === "up"}
+              title="Income"
+              type="up"
+              onPress={() => handleTransactionType("up")}
+            />
+            <TransactionTypeButton
+              isActive={transactionType === "down"}
+              title="Outcome"
+              type="down"
+              onPress={() => handleTransactionType("down")}
+            />
+          </TransactionTypes>
         </Fields>
         <Button title="Enviar" />
       </Form>
