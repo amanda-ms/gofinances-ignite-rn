8f7de8e3894dde682ed20a61b32de9f032a3fe61
Amanda Silva <amanda-ms@hotmail.com>
2021-07-30 15:03:43 -0300

    feat: create component button

--- src/pages/Register/index.tsx
+++ src/pages/Register/index.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Input } from "../../components/Form/Input";
-import { Container, Header, Title, Form } from "./style";
+import { Button } from "../../components/Form/Button";
+import { Container, Header, Title, Form, Fields } from "./style";
 
 export function Register() {
   return (
@@ -10,8 +11,11 @@ export function Register() {
       </Header>
 
       <Form>
-        <Input placeholder="Nome" />
-        <Input placeholder="Preço" />
+        <Fields>
+          <Input placeholder="Nome" />
+          <Input placeholder="Preço" />
+        </Fields>
+        <Button title="Enviar" />
       </Form>
     </Container>
   );
