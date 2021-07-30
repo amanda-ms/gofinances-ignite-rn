2eb2a021f57037f57e6ad3601e266495ad2f7a94
Amanda Silva <amanda-ms@hotmail.com>
2021-07-30 17:49:15 -0300

    feat: initial page category resume

---
+++ src/pages/CategorySelect/index.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import { Container, Header, Title } from "./styles";
+
+export function CategorySelect() {
+  return (
+    <Container>
+      <Header>
+        <Title>Resumo por categoria</Title>
+      </Header>
+    </Container>
+  );
+}
